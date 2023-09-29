446ede9d75cacdc2c44f94f4d170e3ac3551c947
Luis Machaca <luismachaca@outlook.com.pe>
2023-09-28 19:13:36 -0500

    updated alerts when there is an error or the app is reload

--- src/components/contact-list/ContactListRows.jsx
+++ src/components/contact-list/ContactListRows.jsx
@@ -1,10 +1,29 @@
 import './ContactListRows.scss';
 import ContactRow from './ContactRow';
 import ContactCard from './ContactCard';
+import UpdateIcon from '../icons/UpdateIcon';
+import CrossCircleIcon from '../icons/CrossCircleIcon';
 const ContactListRows = ({ contacts, hasError, isLoading, view }) => {
-    if (hasError) return <p>Error loading contacts</p>;
-    if (isLoading) return <span> Loading.....</span>;
-    if (!contacts.length) return <span> Loading.....</span>;
+    if (hasError)
+        return (
+            <p className='ContactListAlert'>
+                <CrossCircleIcon className='ContactListAlert-icon' />
+                Error loading contacts
+            </p>
+        );
+    if (isLoading)
+        return (
+            <p className='ContactListAlert'>
+                <UpdateIcon className='ContactListAlert-icon' /> Loading
+            </p>
+        );
+    if (!contacts.length)
+        return (
+            <p className='ContactListAlert'>
+                <UpdateIcon className='ContactListAlert-icon' />
+                Loading
+            </p>
+        );
     const ContactComponent = view ? ContactRow : ContactCard;
     return (
         <div className='ContactListRows'>
